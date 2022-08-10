a3a565f1ea8208d55a47c153148c51adb9d26cac
john_dev <johnhaimez314@gmail.com>
2022-08-10 23:47:23 +0200

    feat : delete implement test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $roles = Role::all();
-    return view('welcome', ['roles' => $roles]);
+    //$roles = Role::all();
+    return view('welcome');
 });
