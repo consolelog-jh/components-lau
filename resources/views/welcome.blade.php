a44945d75e146c076439e8df751edefb8e591a25
john_dev <johnhaimez314@gmail.com>
2022-08-10 23:26:38 +0200

    feat : create files for auth's fortify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,8 @@
 
 @section('app-content')
     <x-titles.h1>Accueil</x-titles.h1>
-
     <x-titles.h2>Utilisateur formulaire</x-titles.h2>
-    <x-user-forms.register/>
+    <x-user-forms.register :roles="$roles"/>
     <x-user-forms.login/>
     <x-user-forms.logout>Deconnection</x-user-forms.logout>
     <x-user-forms.verify-email/>
