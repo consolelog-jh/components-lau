2f5e1c2e13e09b59b54ee59c5f34c20c7329a9be
john_dev <johnhaimez314@gmail.com>
2022-08-10 23:47:49 +0200

    feat : lastest component select input

--- resources/views/components/inputs/select.blade.php
+++ resources/views/components/inputs/select.blade.php
@@ -1,10 +1,21 @@
 <div class="text-black mt-4 mb-8">
     {{-- if label exist --}}
     @if($label)
-        <label class="text-lg" for="{{ $label . $name }}">{{ $label }}</label>
+        <label class="
+            block 
+            mb-2 
+            text-labelinput
+            dark:text-labelinputd
+            text-s-labelinput 
+            {{-- your custom class --}}
+            " 
+            for="{{ $label . $name }}"
+        >
+            {{ $label }}
+        </label>
     @endif
 
-    <div class="mt-2 relative bg-inputbg dark:bg-inputbgd rounded-lg">
+    <div class="mt-2 relative bg-inputbg dark:bg-inputbgd rounded-lg {{-- your custom class --}}">
         {{-- selected for input --}}
         <select 
         {{ $attributes }} 
@@ -17,16 +28,17 @@
             px-4 
             w-full  
             outline-none
+            {{-- your custom class --}}
             " 
             name="{{ $name }}" 
             id="{{ $label . $name }}"
         >
-            <option class="py-4 px-4 text-lg" value="">{{ $placeholder }}</option>
+            <option class="py-4 px-4 {{-- your custom class --}}" value="">{{ $placeholder }}</option>
             @if ($entities)
                 @foreach ($entities as $key => $entitie)
                     <option 
                         @if($selected == $entitie->libelle) selected @endif 
-                        class="py-4 px-4 text-lg" 
+                        class="py-4 px-4 {{-- your custom class --}}" 
                         value="{{ $entitie->id }}"
                     >
                         {{ $entitie->libelle }}
@@ -40,4 +52,31 @@
             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
     </div>
+
+    {{-- message error of input --}}
+    @error($name)
+        <div class="fit flex items-center">
+            {{-- icon error --}}
+            <x-icons.error 
+                classdiv="
+                text-inputerror 
+                dark:text-inputerrord
+                {{-- custom icon error here --}}
+                " 
+                w="w-4" 
+                h="w-4"
+            />
+
+            {{-- message --}}
+            <p class="
+            text-inputerror 
+            dark:text-inputerrord
+            text-s-inputerror
+            {{-- custom message here --}}
+            "
+            >
+                {{ $message }}
+            </p>
+        </div>
+    @enderror
 </div>
