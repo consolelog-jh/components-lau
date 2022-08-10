bf4cd3c04feebf05c1063ab503e6f3a213328c1d
john_dev <johnhaimez314@gmail.com>
2022-08-10 15:40:47 +0200

    feat : test components in /

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 
 @section('app-content')
+    <x-titles.h1>Accueil</x-titles.h1>
+
+    <x-titles.h2>Utilisateur formulaire</x-titles.h2>
+    <x-user-forms.register/>
+    <x-user-forms.login/>
+    <x-user-forms.logout>Deconnection</x-user-forms.logout>
+    <x-user-forms.verify-email/>
+    <x-user-forms.update-profil/>
+    <x-user-forms.reset-password/>
+    <x-user-forms.forgot-password/>
 @endsection
