c790b16f30448b800b6d2b0edc5e05dcb165cd18
john_dev <johnhaimez314@gmail.com>
2022-08-11 12:04:58 +0200

    feat : add mode dark and create component btn mode dark

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 import './bootstrap';
 import 'unpoly';
 import 'unpoly/unpoly.css';
+import DarkMode from'./darkmode';
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
-Alpine.start()
+Alpine.start()  
+
+// when loading check mode dark with device
+//DarkMode.whenLoadingAddModeDarkIfExist()
+
+// when loading check mode dark without device
+DarkMode.whenLoadingAddModeDarkIfExist(false)
+
+// switch mode dark  
+DarkMode.switchModeDark()
